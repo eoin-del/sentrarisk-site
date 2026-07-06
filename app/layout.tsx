87cdead56815632b75c994b49c4fa9b2819888e7
Eoin Carey <eoincarey@Eoins-MacBook-Air.local>
2026-07-06 22:48:32 +0100

    Suppress HubSpot hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased">
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
       <head>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-VS527Y2NFS" />
         <script
